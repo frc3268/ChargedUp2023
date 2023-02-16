048a7878a84debaa683ca77fca52d0fa42b78d1a
Matthew Epshtein <robotmeatsuit@gmail.com>
2023-02-16 09:31:56 -0800

    feed camera res to a var

--- src/main/kotlin/frc/robot/subsystems/CameraSubsystem.kt
+++ src/main/kotlin/frc/robot/subsystems/CameraSubsystem.kt
@@ -1,12 +1,19 @@
 package frc.robot.subsystems
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import org.photonvision.PhotonCamera;
+import org.photonvision.targeting.PhotonPipelineResult
+
 
 class CameraSubsystem : SubsystemBase() {
-    PhotonCamera cam = PhotonCamera();
+    val cam:PhotonCamera = PhotonCamera("CCP SPY BALOON")
+    var frame:PhotonPipelineResult? = null
+    
+
 
     override fun periodic() {
         // This method will be called once per scheduler run
+        frame = cam.getLatestResult()
     }
 
     override fun simulationPeriodic() {
